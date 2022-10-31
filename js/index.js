b9735fe929fd7185b88b478809af13a25f56deeb
Orlando Suarez <corxosz@gmail.com>
2022-10-31 00:39:03 -0600

    creacion de branch development, ajustes generales y en imagenes. Se inicio toggle pero aun le falta

--- js/index.js
+++ js/index.js
@@ -3,3 +3,19 @@ window.addEventListener("scroll", () => {
     let header = this.document.querySelector(".header");
     header.classList.toggle("menu-sticky",window.scrollY>100);
 });
+
+/**-----------Toggle------------**/
+/* const accordion = document.querySelector(".toggle-contenido");
+const accordionItem = document.querySelector(".toggle-item");
+let height = 0;
+
+accordionItem.addEventListener("click", () => {
+    if(height) {
+        height = accordion.scrollHeight;
+    }else{
+        height = 0;
+    }
+
+    accordion.style.height = `${height}px`;
+
+}); */
